818299f616eefe142f633b60380fc9fe2418fc3b
Pablo Martínez <pablo@Pablos-MacBook-Pro.local>
2020-01-28 11:03:29 +0100

    Removed dist package

--- dist/common/constants.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.HIGHWAY_ENDPOINT = "http://localhost:8090/api";
-exports.API_VERSION = "v1";
-exports.USER_AGENT = "SmartMonkeyApiClientJS";
-exports.FLAKE_OPTIMIZE = exports.HIGHWAY_ENDPOINT + "/" + exports.API_VERSION + "/" + "optimize";
-//# sourceMappingURL=constants.js.map
--- dist/common/schemas.d.ts
+++
@@ -1,9 +0,0 @@
-import Joi from 'joi';
-export declare const VehicleSchema: Joi.ObjectSchema;
-export declare const ServiceSchema: Joi.ObjectSchema;
-export declare const RewardRegionSchema: Joi.ObjectSchema;
-export declare const VehicleListSchema: Joi.ArraySchema;
-export declare const ServiceListSchema: Joi.ArraySchema;
-export declare function validateVehicles(vehicles: any): void;
-export declare function validateServices(services: any): void;
-export declare function validateRewardRegions(rewardRegions: any): void;
