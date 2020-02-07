cd882caf5dd68a226e63414607e816da901805ea
Pablo Martínez <pablo@Pablos-MacBook-Pro.local>
2020-02-07 19:57:44 +0100

    removed async

--- lib/src/Service.ts
+++ lib/src/Service.ts
@@ -24,7 +24,7 @@ export class Service {
     return response;
   };
 
-  fromClient = async (client: IClient) => {
+  fromClient = (client: IClient) => {
     const { label, location, tags, comments, phone, email, website } = client;
     const newService: IServiceData = {
       label, location, tags, comments,
