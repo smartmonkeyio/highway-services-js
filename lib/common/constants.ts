8256e4a75dc413ed432b8c8fcb31f1fc509a255c
Pablo Martinez <pablo.martinez@smartmonkey.io>
2020-03-02 13:55:26 +0100

    Fixed build tests

--- lib/common/constants.ts
+++ lib/common/constants.ts
@@ -3,6 +3,6 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 export const HIGHWAY_ENDPOINT =
-  process.env.HIGHWAY_ENDPOINT || `https://highwayservices.smartmonkey.io/api`;
+  process.env.HIGHWAY_ENDPOINT || `https://highwayservices.smartmonkey.io`;
 export const API_VERSION = process.env.API_VERSION || `v1`;
 export const USER_AGENT = process.env.USER_AGENT || `SmartMonkeyApiClientJS`;
--- test/Plan.spec.ts
+++ test/Plan.spec.ts
@@ -8,46 +8,46 @@ import { Highway } from '../lib/src/Highway';
 describe(`Test Plans API`, () => {
     let highway: Highway;
     const allPlanIds: string[] = [];
+    let plan: any;
     before(() => {
         highway = createHighway(common.key);
     });
     describe(`Basic Plan CRUD`, () => {
         it(`it should create a new Plan`, async () => {
-            const plan = await highway.plan.create({});
+            plan = await highway.plan.create({});
             assert.strictEqual(plan._version, 1);
             assert.strictEqual(plan.label, undefined);
             assert.notStrictEqual(plan.created_at, undefined);
             allPlanIds.push(plan.id);
         });
         it(`it should be able to retrieve the recently created plan`, async () => {
-            const plan = await highway.plan.get(allPlanIds[0]);
+            plan = await highway.plan.get(allPlanIds[0]);
             assert.strictEqual(plan._version, 1);
             assert.strictEqual(plan.label, undefined);
             assert.notStrictEqual(plan.created_at, undefined);
-            allPlanIds.push(plan.id);
         });
         it(`it should be able to update the plan`, async () => {
-            const plan = await highway.plan.update(allPlanIds[0], { label: `manolo` });
+            plan = await highway.plan.update(allPlanIds[0], { label: `manolo` });
             assert.strictEqual(plan._version, 1);
             assert.strictEqual(plan.label, `manolo`);
         });
         it(`Optimize must do nothig with an empty list`, async () => {
-            const plan = await highway.plan.optimize(allPlanIds[0]);
+            plan = await highway.plan.optimize(allPlanIds[0]);
             assert.strictEqual(plan._version, 1);
             assert.strictEqual(plan.label, `manolo`);
             assert.strictEqual(plan.services.length, 0);
             assert.strictEqual(plan.routes.length, 0);
         });
         it(`Must be able to create services for a plan`, async () => {
-            const plan = await highway.plan.addServices(allPlanIds[0], [{}, {}]);
+            plan = await highway.plan.addServices(allPlanIds[0], [{}, {}]);
             assert.strictEqual(plan.services.length, 2);
         });
         it(`Must be able to create routes for a plan`, async () => {
-            const plan = await highway.plan.addRoutes(allPlanIds[0], [{}, {}]);
+            plan = await highway.plan.addRoutes(allPlanIds[0], [{}, {}]);
             assert.strictEqual(plan.routes.length, 2);
         });
         it(`Must be able to optimize routes of the current plan`, async () => {
-            const plan = await highway.plan.optimize(allPlanIds[0]);
+            plan = await highway.plan.optimize(allPlanIds[0]);
             assert.strictEqual(plan.routes.length, 2);
             assert.strictEqual(plan.services.length, 2);
         });
